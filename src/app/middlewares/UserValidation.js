d7862affa89e3fa3cc5766c61fa0d8d278dca95f
CamiloSilvaD <camilodsmp@gmail.com>
2019-08-21 10:04:10 -0300

    create the user validation middleware and JWT authentication. Create the update and store user controller methods

---
+++ src/app/middlewares/UserValidation.js
@@ -0,0 +1,45 @@
+import * as Yup from 'yup';
+
+class UserValidation {
+  async store(req, res, next) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    return next();
+  }
+
+  async update(req, res, next) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    return next();
+  }
+}
+
+export default new UserValidation();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import UserValidation from './app/middlewares/UserValidation';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
+routes.post('/users', UserValidation.store, UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserValidation.update, UserController.update);
 
 export default routes;
