89ae902e7172094258f1ead23b3eb38e22b9e58f
CamiloSilvaD <camilodsmp@gmail.com>
2019-08-23 10:51:03 -0300

    Create multer configuration and environment variables files. Update the .gitignore file.

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
@@ -7,6 +9,7 @@ import UserValidation from './app/middlewares/UserValidation';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserValidation.store, UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -14,5 +17,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserValidation.update, UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json();
+});
 
 export default routes;
